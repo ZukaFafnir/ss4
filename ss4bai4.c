5925fd76f2a966590015cd81562373b8b7e6d3f0
Nguyen Van Minh Tri <mtanime020705@gmail.com>
2024-12-14 18:13:14 +0700

    Add files via upload

---
+++ ss4bai4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int month;
+	scanf("%d",&month);
+	switch(month){
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			printf("this month has 31 days");
+			break;
+		case 4: case 6: case 9: case 11:
+		    printf("this month has 30 days");
+		    break;
+		case 2:
+			printf("this month has 28 days");
+			break;
+		default:
+			printf("invalid data!");
+	}
+		return 0;
+}
