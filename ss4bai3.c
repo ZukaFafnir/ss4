b699810b2d44f3544fe9b2fef3f71720ddc3fab4
Nguyen Van Minh Tri <mtanime020705@gmail.com>
2024-12-17 19:37:10 +0700

    Add files via upload

--- ss4bai3.c
+++ ss4bai3.c
@@ -3,21 +3,20 @@ int main(){
 	int number;
 	scanf("%d",&number);
 	
-	if(number %3==0){
-		if(number %5==0){
-			printf("this number divisible by 3,5");
-		}
-		else{
-			printf("this number divisible by 3 and indivisible by 5");
-		}
-	}
-	else{
-		if(number %5==0){
+	if(number %3==0 && number %5==0){
+        
+        printf("this number divisible by 3,5");
+		} 
+	
+	else if(number %5==0){
 			printf("this number indivisible by 3 and divisible 5 ");
 		}
-		else{
+	     else if(number %5==0){
+			printf("this number divisible by 3 and indivisible 5 ");
+	    }
+		       else{
 			printf("this number indivisible by 3 and 5");
 		}
-	}
+	
 	return 0;
 }
